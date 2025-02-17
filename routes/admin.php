f8bcb973dae172a0fdbe4ebf4fd164579ee3848d
gilang <gilangprtm210498@gmail.com>
2025-02-17 12:29:36 +0800

    model publisher

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PublisherController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
+    //categori
     Route::controller(CategoryController::class)->group(function () {
         Route::get('/categories', 'index')->name('admin.categories.index');
         Route::get('/categories/create', 'create')->name('admin.categories.create');
@@ -11,5 +13,15 @@ Route::middleware(['auth'])->prefix('admin')->group(function () {
         Route::get('/categories/edit/{category}', 'edit')->name('admin.categories.edit');
         Route::put('/categories/edit/{category}', 'update')->name('admin.categories.update');
         Route::delete('/categories/destroy/{category}', 'destroy')->name('admin.categories.destroy');
+    });
+
+    //publisher
+    Route::controller(PublisherController::class)->group(function () {
+        Route::get('/publishers', 'index')->name('admin.publishers.index');
+        Route::get('/publishers/create', 'create')->name('admin.publishers.create');
+        Route::post('/publishers/create', 'store')->name('admin.publishers.store');
+        Route::get('/publishers/edit/{publisher}', 'edit')->name('admin.publishers.edit');
+        Route::put('/publishers/edit/{publisher}', 'update')->name('admin.publishers.update');
+        Route::delete('/publishers/destroy/{publisher}', 'destroy')->name('admin.publishers.destroy');
     });
 });
